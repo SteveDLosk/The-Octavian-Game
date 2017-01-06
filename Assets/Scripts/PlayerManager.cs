dc6098fa8ded214d3d45d3373775379a2cb52a47
Steve Losk <SteveDLosk@gmail.com>
2017-01-05 18:28:45 -0800

    Bounce back from contact with enemy
    moves the character slightly away from enemy on collision.  Seems to
    keep player from getting stuck on enemy.

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -10,9 +10,12 @@ public class PlayerManager : MonoBehaviour
     public int score { get; set; }
     public int health { get; set; }
 
+    Rigidbody2D rb;
+
     void Start()
     {
         health = 30;
+        rb = GetComponent<Rigidbody2D>();
     }
     // Player damage
     void OnCollisionEnter2D(Collision2D other)
@@ -20,7 +23,14 @@ public class PlayerManager : MonoBehaviour
         // Check if hits an enemy, and if player is a currently valid target
         if (other.gameObject.tag == "Enemy" && gameObject.tag == "Player")
         {
-            TakeDamage(5);        
+            TakeDamage(5);
+
+            // Bounce back from enemy
+            float x = gameObject.transform.position.x - other.transform.position.x;
+            float y = gameObject.transform.position.y - other.transform.position.y;
+            Vector2 bounce = new Vector2((x * 10),(y * 10));
+            rb.AddForce(bounce);
+           
         }
 
     }
