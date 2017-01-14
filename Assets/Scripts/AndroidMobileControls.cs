1380790f1bc4451473cee654103c08826d6dfa09
Steve Losk <SteveDLosk@gmail.com>
2017-01-13 19:12:15 -0800

    Android Mobile script
    Added functions for left, right, and jump

---
+++ Assets/Scripts/AndroidMobileControls.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndroidMobileControls : MonoBehaviour
+{
+    float speed;
+    public float speedX;
+    public bool Grounded;
+    public bool Jumping;
+    public Rigidbody2D rb;
+    public float jumpSpeedY;
+    // Use this for initialization
+    void Start ()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void PressLeft()
+    {
+        speed = -speedX;
+    }
+
+    void ReleaseLeft()
+    {
+        speed = 0;
+    }
+
+    void PressRight()
+    {
+        speed = speedX;
+    }
+
+    void ReleaseRight()
+    {
+        speed = 0;
+    }
+
+    void Jump()
+    {
+        if (Grounded)
+        {
+            Jumping = true;
+            rb.AddForce(new Vector2(rb.velocity.x, jumpSpeedY));
+
+            //  anim.SetInteger("State", 3);
+
+            // Play jump sound
+            AudioSource audio = GetComponent<AudioSource>();
+
+            audio.Play();
+            audio.Play(44100);
+        }
+    }
+
+
+}
